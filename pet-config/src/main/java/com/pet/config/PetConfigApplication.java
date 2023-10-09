d9821f5548b6a7dbf0679075f79c41e98dd75ba0
0oc <you@example.com>
2023-10-10 00:36:10 +0900

    Build a sample project with spring cloud using cloud config, eureka, zuul, feign, hystrix and turbine

--- pet-config/src/main/java/com/pet/config/PetConfigApplication.java
+++ pet-config/src/main/java/com/pet/config/PetConfigApplication.java
@@ -3,9 +3,12 @@ package com.pet.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class PetConfigApplication {
 
 	public static void main(String[] args) {
